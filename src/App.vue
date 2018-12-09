bf4aa67d7cf035c29d34b211c879c676885c929e
oki2a24 <oki2a24@gmail.com>
2018-12-09 10:42:26 +0900

    feat: アプリの準備として土台となるファイルを用意する

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- ここに実装を書いていく -->
+    <p>{{ msg }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  // ここに実装を書いていく
+  data () {
+    return {
+      msg: 'こんにちは！こんちには！'
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+// ストアの定義
+const store = new Vuex.Store({
+})
+
+// ストアをエクスポート
+export default store
