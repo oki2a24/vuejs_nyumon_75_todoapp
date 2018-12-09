a22f2e1fac21ddfd6b097a8c5402f1552c433321
oki2a24 <oki2a24@gmail.com>
2018-12-09 11:25:35 +0900

    feat: 7.5.3 タスクの新規作成と完了

--- src/store.js
+++ src/store.js
@@ -17,8 +17,37 @@ const store = new Vuex.Store({
         name: 'Vue.jsの本を買う',
         done: true
       }
-    ]
-  }
+    ],
+
+    // 次に追加するタスクのID
+    // 実際のアプリではサーバで生成したり、UUIDを使ったりするがここでは決め打ち
+    nextTaskId: 3,
+  },
+
+  mutations: {
+    // タスクを追加する
+    addTask (state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      })
+
+      // 次に追加されるタスクに付与するIDを更新する
+      state.nextTaskId++
+    },
+
+    // タスクの完了状態を変更する
+    toggleTaskStatus (state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+
+      filtered.forEach(task => {
+        task.done = !task.done
+      })
+    },
+  },
 })
 
 // ストアをエクスポート
